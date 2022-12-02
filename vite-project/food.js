8ea227510f87897e91821b50f22b892816101486
AndrewL777 <7landrew7@gmail.com>
2022-12-02 13:58:25 -0500

    211122132132321231

---
+++ vite-project/food.js
@@ -0,0 +1,65 @@
+const foods = [
+  {
+    name: "Egg",
+    vegan: "",
+    price: "blue",
+    locker: ["142"],
+  },
+  {
+    name: "Bob",
+    animal: "lobster",
+    color: "red",
+    device: "phone",
+    locker: ["54"],
+  },
+  {
+    name: "Jake",
+    animal: "cow",
+    color: "phone",
+    device: "laptop",
+    locker: ["643"],
+  },
+  {
+    name: "Harry",
+    animal: "fish",
+    color: "orange",
+    device: "phone",
+    locker: ["16"],
+  },
+  {
+    name: "Link",
+    animal: "lamb",
+    color: "green",
+    device: "laptop",
+    locker: ["2"],
+  },
+  {
+    name: "Anson",
+    animal: "duck",
+    color: "purple",
+    device: "phone",
+    locker: ["72"],
+  },
+  {
+    name: "Jayson",
+    animal: "crab",
+    color: "orange",
+    device: "laptop",
+    locker: ["73"],
+  },
+  {
+    name: "Taylor",
+    animal: "shrimp",
+    color: "black",
+    device: "macbook",
+    locker: ["42"],
+  },
+  {
+    name: "Hunter",
+    animal: "oyster",
+    color: "white",
+    device: "ipad",
+    locker: ["13"],
+  },
+];
+foods;
