4c391962d5c30b9621f1c3ccb5556ff471b3009b
AndrewL777 <7landrew7@gmail.com>
2022-12-06 13:58:46 -0500

    1`21`2`2

--- vite-project/index.js
+++ vite-project/index.js
@@ -1,14 +1,1 @@
 import "/style.css";
-
-document.querySelector("#app").innerHTML = `
-  
-<h1>Hwi</h1>
-<div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    
-`;
