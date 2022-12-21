993cafd0e8c44bf8c5a636e184a86c8ad695076d
AndrewL777 <7landrew7@gmail.com>
2022-12-21 13:21:35 -0500

    ---------------

--- vite-project/dom.js
+++ vite-project/dom.js
@@ -5,3 +5,19 @@ const DOMSelectors = {
   button: document.getElementById("btn"),
 };
 console.log(foods);
+
+function card() {
+  DOMSelectors.form.insertAdjacentHTML(
+    "afterend",
+    `
+    <div class="card"> 
+    <img class src="${DOMSelectors.foods.img.value}" /><img>
+    <h2 class="artist">${DOMSelectors.foods.name.value}</h2>
+    <h3 class="title">${DOMSelectors.foods.price.value}</h3>
+    <button class="remove btn">Remove</button>
+    </div>
+    `
+  );
+}
+
+//   <div class="display-card">   </div> PUT IN HTML!!!!!
